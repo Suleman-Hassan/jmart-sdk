c5bf536681fbd543d0750dda25435067a846a071
Suleman Hassan <hassansalmanayub1@gmail.com>
2026-02-11 11:14:19 +0500

    feat: sdk parameter initialization done

--- lib/jmart_sdk.dart
+++ lib/jmart_sdk.dart
@@ -1,20 +1,46 @@
+// library jmart_sdk;
+//
+// import 'package:flutter/material.dart';
+// import 'src/main.dart';
+//
+// class JMartSDK {
+//   static Future<void> launch(BuildContext context) async {
+//     await Navigator.of(context).push(
+//       MaterialPageRoute(
+//         builder: (context) => MyApp(),
+//       ),
+//     );
+//     // await Navigator.of(context).push(
+//     //   MaterialPageRoute(
+//     //     fullscreenDialog: true,
+//     //     builder: (context) => MyApp(),
+//     //   ),
+//     // );
+//   }
+// }
+
 library jmart_sdk;
 
 import 'package:flutter/material.dart';
-import 'src/main.dart';
+import 'src/pages/askari_end_point/askari_end_point.dart';
 
 class JMartSDK {
-  static Future<void> launch(BuildContext context) async {
+  static Future<void> launch(
+      BuildContext context, {
+        String? userEmail,
+        String? firstName,
+        String? lastName,
+        String? phoneNumber,
+      }) async {
     await Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => MyApp(),
+        builder: (context) => AskariEndPoint(
+          userEmail: userEmail,
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+        ),
       ),
     );
-    // await Navigator.of(context).push(
-    //   MaterialPageRoute(
-    //     fullscreenDialog: true,
-    //     builder: (context) => MyApp(),
-    //   ),
-    // );
   }
 }
