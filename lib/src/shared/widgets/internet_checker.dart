98464992e56b9a028f1067c788bed9ce63bc93d7
Suleman Hassan <hassansalmanayub1@gmail.com>
2026-05-21 11:47:57 +0500

    Feat: dependency updated

--- lib/src/shared/widgets/internet_checker.dart
+++ lib/src/shared/widgets/internet_checker.dart
@@ -1,13 +1,12 @@
-import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'dart:async';
 
 class InternetChecker {
-  StreamSubscription<InternetConnectionStatus>? _listener;
+  StreamSubscription<InternetStatus>? _listener;
 
   void startListening() {
-    final checker = InternetConnectionChecker.instance;
-    _listener = checker.onStatusChange.listen((status) {
-      if (status == InternetConnectionStatus.connected) {
+    _listener = InternetConnection().onStatusChange.listen((status) {
+      if (status == InternetStatus.connected) {
         print("✅ Internet is connected.");
       } else {
         print("❌ No internet connection.");
